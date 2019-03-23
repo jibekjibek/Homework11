0cbcf5b886019316c9ec7391450d9bf25b2fe939
JibekJibek <tadzhibayevajbk@gmail.com>
2019-03-23 20:20:18 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import javax.naming.Name;
+import javax.print.DocFlavor;
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args) {
+            final String HI;
+            String name;
+            HI = "Hello, ";
+            name = "world!";
+
+            System.out.print(HI);
+            System.out.println(name);
+
+
+
+        }
+}
+
+
